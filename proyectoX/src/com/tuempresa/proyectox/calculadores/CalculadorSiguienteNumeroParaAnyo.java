2b644ee127521549e1447cacb1cbb8e3a5c2441a
AyalaManu <114090237+AyalaManu@users.noreply.github.com>
2022-10-13 17:45:12 -0500

    Leccion14
    Lecicion 1-4

---
+++ proyectoX/src/com/tuempresa/proyectox/calculadores/CalculadorSiguienteNumeroParaAnyo.java
@@ -0,0 +1,28 @@
+package com.tuempresa.proyectox.calculadores;
+
+import javax.persistence.*;
+
+import org.openxava.calculators.*;
+import org.openxava.jpa.*;
+
+import lombok.*;
+
+public class CalculadorSiguienteNumeroParaAnyo 
+	implements ICalculator{
+	
+	@Getter @Setter
+	int anyo;
+
+	@Override
+	public Object calculate() throws Exception {
+		Query query = XPersistence.createManager()
+				.createQuery("select max(f.numero) from Factu f where f.anyo = :anyo");
+				query.setParameter("anyo",anyo);
+				Integer ultimoNumero = (Integer) query.getSingleResult();
+		return ultimoNumero == null ? 1 : ultimoNumero + 1;
+	}
+		
+	}
+	
+
+
